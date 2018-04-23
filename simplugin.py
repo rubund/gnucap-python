e651213e34f2a94d412d86e679a5830d7ec0fd8c
Felix Salfelder <felix@salfelder.org>
2018-04-23 21:16:57 +0100

    add stdout.flush in test
    work around python3 print buffer
    (why is this not flusing at exit?)

--- simplugin.py
+++ simplugin.py
@@ -5,13 +5,17 @@
 # load from gnucap with "python simplugin.py",
 # then run "mysim".
 
-from gnucap import install_command, SIM
+from gnucap import install_command, SIM #, IO
+import sys
 
 class mysim(SIM):
 	def do_it(self, cmd, scope):
 		print("running mysim...")
+		sys.stdout.flush()
 	def setup(self, cmd):
 		pass
+		#out = IO.mstdout;
+		#out.reset(); # BUG// don't know why this is needed
 	def sweep(self):
 		pass
 
@@ -29,3 +33,5 @@ d3 = install_command("mysim", sim)
 #BUG: that does not work as expected.
 # it just breaks "mysim". why?
 #d3 = install_command("mysim", sim)
+
+sys.stdout.flush()
